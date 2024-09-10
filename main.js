d44d6d5de1e45fef72b1eed7b940cad75bed822c
Asad-Malik2007 <167566770+Asad-Malik2007@users.noreply.github.com>
2024-09-10 21:28:28 +0500

    Update main.js

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ container.style.display="none"
 async function checkWeather(city){
   const response=await fetch(apiUrl+city+`&appid=${apiKey}`);
   var data = await response.json();
-     
+  data.name.trimEnd()
   document.querySelector(".city").innerHTML = data.name;
   document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
   document.querySelector(".humidity").innerHTML = data.main.humidity + " %";
